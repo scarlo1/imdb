9ce1da02f1c3905cc083af07ecca9277d2fb2750
Carlos flores <scarlo1@hotmail.com>
2023-03-26 20:27:00 -0500

    fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,20 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+export const dynamic = "force-dynamic"; // this is the fix
+import Results from "@/components/Results";
 
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`, { next: { revalidate: 10000 }});
 
-export default function Home() {
-  return (
-    <h1 className="text-blue-900">HOME</h1>
-  )
+  if(!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+  return ( 
+  <div>
+    <Results results={results} />
+  </div>
+  );
 }
